658469a94000dadd89e73f0693a255d46c2155e8
Noel Gonzalez <ngo.lia.uaemex@gmail.com>
2025-09-18 02:32:22 -0600

    Commit for boilerplate genereted code

---
+++ src/main/java/com/ngo/customers/data/CustomerDataMsApplication.java
@@ -0,0 +1,13 @@
+package com.ngo.customers.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerDataMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerDataMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ngo/customers/data/CustomerDataMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngo.customers.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerDataMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
